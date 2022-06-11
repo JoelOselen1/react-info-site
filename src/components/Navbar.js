2ec714b2f37e41b15101f3d78915861edcdd9714
JoelOselen1 <joeloselen@gmail.com>
2022-06-11 11:19:20 +0100

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import react from 'react';
+export default function Navbar(){
+  return (
+     <nav>
+     <img src='./react-icon-small.png' className='nav--icon' />
+     <h3 className='nav--logo_text'>ReactFacts</h3>
+     <h4 className='nav--title'>React course - project 1</h4>
+     </nav>
+    );
+}
